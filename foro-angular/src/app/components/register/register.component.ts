0386e07dbf5d959606834a83343f83fa455158ff
Álvaro Torrente Pérez <alvarotorrenteperez98@gmail.com>
2019-11-12 21:49:14 +0100

    finalizado

---
+++ foro-angular/src/app/components/register/register.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+import { Observable } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
+import { ThrowStmt } from '@angular/compiler';
+
+@Component({
+  selector: 'register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+  providers: [UserService]
+})
+export class RegisterComponent implements OnInit {
+  public page_title: string;
+  public user: User;
+  public status: string;
+  constructor(
+    private _userService: UserService
+  ) { 
+    this.page_title= "Registrate";
+    this.user= new User('','','','','','','ROLE_USER');
+  }
+
+  ngOnInit() {
+    console.log(this._userService.test());
+  }
+
+  onSubmit(form){
+    this._userService.register(this.user).subscribe(
+      response =>{
+        if(response.user && response.user._id){
+          this.status='success';
+          form.reset();
+        }else{
+          this.status='error';
+        }
+
+      },
+      err=>{
+        console.log(err)
+      }
+    );
+  }
+
+  
+
+}
